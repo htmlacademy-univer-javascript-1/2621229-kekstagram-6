cdcdde7013aa012f9c030dfe9aca8a310b2f7cf9
d8362721-code <d8362721@gmail.com>
2025-10-16 17:04:01 +0500

    Update functions.js

--- js/functions.js
+++ js/functions.js
@@ -1,1 +1,22 @@
+function isMeetingWithinWorkday(workStart, workEnd, meetingStart, duration) {
+  function timeToMinutes(timeStr) {
+    let parts = timeStr.split(':');
+    let hours = parseInt(parts[0], 10);
+    let minutes = parseInt(parts[1], 10);
+    return hours * 60 + minutes;
+  }
+
+  let workStartMin = timeToMinutes(workStart);
+  let workEndMin = timeToMinutes(workEnd);
+  let meetingStartMin = timeToMinutes(meetingStart);
+  let meetingEndMin = meetingStartMin + duration;
+
+  return meetingStartMin >= workStartMin && meetingEndMin <= workEndMin;
+}
+
+console.log(isMeetingWithinWorkday('08:00', '17:30', '14:00', 90));
+console.log(isMeetingWithinWorkday('8:0', '10:0', '8:0', 120));
+console.log(isMeetingWithinWorkday('08:00', '14:30', '14:00', 90));
+console.log(isMeetingWithinWorkday('14:00', '17:30', '08:0', 90));
+console.log(isMeetingWithinWorkday('8:00', '17:30', '08:00', 900));
 
